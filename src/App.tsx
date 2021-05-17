b1c55881c61e58c4000b39cf595ac08491a6392e
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-05-17 01:06:03 -0300

    add lib for icons and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+
+import Login from './pages/Login';
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Fragment>
-      <h1>Hello World</h1>
+      <Login />
       <GlobalStyle />
     </Fragment>
   );
